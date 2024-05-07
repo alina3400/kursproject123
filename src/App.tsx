85329f763f76fcb3030f6b9f49c0731e344f4ef0
Alina <linalina3400@mail.ru>
2024-05-07 10:35:00 +0300

    pages&components

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,30 @@ import { ProductPage } from "./Pages/ProductPage";
 import { Help } from "./Pages/Help";
 import { CompanyPage } from "./Pages/CompanyPage";
 import { Errors } from "./Pages/Errors";
+import { Navbar } from "./Components/Navbar";
+import { Footer } from "./Components/Footer";
 
 
 export default function Example() {
   const [data, setData] = useState([])
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
+    fetch('https://openlibrary.org/works/OL45804W/editions.json')
     .then( res => res.json())
     .then( data => setData(data))
     .catch( err => console.log(err));
   }, [])
   return <>
+  <Navbar/>
 <BrowserRouter>
  <Routes>
    <Route path="/" element={<Home/>}/>                            //Главная страница 
    <Route path="/books" element={<Books/>}/>                      //Страница с книгами
    <Route path="/product/:proid" element={<ProductPage />} />     //Книга отдельно
    <Route path="/help" element={<Help/>}/>                        //Поддержка
-   <Route path="/app" element={<CompanyPage/>}/>              //О приложении
+   <Route path="/app" element={<CompanyPage/>}/>                  //О приложении
    <Route path="*" element={<Errors/>}/>
  </Routes>
  </BrowserRouter>
+ <Footer/>
   </>
 }
