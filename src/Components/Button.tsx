3b364139b3cf2aa1e5f3b7d9f80c9e5d15ad106a
Alina <linalina3400@mail.ru>
2024-05-10 22:08:10 +0300

    pages&components

---
+++ src/Components/Button.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export function Button(){
+    const [active, setActive] = useState(false);
+  const handleClick = () => {
+    setActive(!active);
+  };
+  const var1 = "bg-lime-400 relative py-1 text-center h-10 w-24 max-w-48 font-light hover:bg-lime-600 rounded-full";
+  const var2 = "bg-gray-300 relative py-1 text-center h-10 w-24 max-w-48 font-light hover:bg-gray-500 rounded-full";
+    return<>
+    <div className="mt-4">
+      <button
+        onClick={handleClick}
+        className={active ? var1 : var2 }>
+        Войти
+      </button>
+    </div>
+    </>
+}
